adaad6e81af1ff9a0d078c513599a537d102011f
Chaitanya Shahare <shaharechaitanya3@gmail.com>
2024-04-09 17:50:23 +0530

    feat: used pipes and class-validator for validation

--- src/ninjas/dto/create-ninja.dto.ts
+++ src/ninjas/dto/create-ninja.dto.ts
@@ -1,4 +1,10 @@
+import { MinLength, IsEnum } from 'class-validator';
+
+
 export class CreateNinjaDto {
+  @MinLength(3)
   name: string;
+
+  @IsEnum(['stars', 'nunchucks'], { message: 'Use correct weapon!' })
   weapon: 'stars' | 'nunchucks';
 }
--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -5,9 +5,11 @@ import {
   Get,
   NotFoundException,
   Param,
+  ParseIntPipe,
   Post,
   Put,
   Query,
+	ValidationPipe,
 } from '@nestjs/common';
 import { CreateNinjaDto } from './dto/create-ninja.dto';
 import { UpdateNinjaDto } from './dto/update-ninja.dto';
@@ -25,9 +27,9 @@ export class NinjasController {
 
   // GET /ninjas/:id
   @Get(':id')
-  getOneNinja(@Param('id') id: string) {
+  getOneNinja(@Param('id', ParseIntPipe) id: number) {
     try {
-      return this.ninjasService.getNinja(+id);
+      return this.ninjasService.getNinja(id);
     } catch (err) {
       throw new NotFoundException();
     }
@@ -35,7 +37,7 @@ export class NinjasController {
 
   // POST /ninjas
   @Post()
-  createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+  createNinja(@Body(new ValidationPipe()) createNinjaDto: CreateNinjaDto) {
     return this.ninjasService.createNinja(createNinjaDto);
   }
 
